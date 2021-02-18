3a1a3210db30021f45031230bab902f211e5adb3
asireddyvivek <59225370+asireddyvivek@users.noreply.github.com>
2021-02-18 11:48:04 +0530

    Update FindPeakElement.java

--- FindPeakElement.java
+++ FindPeakElement.java
@@ -1,1 +1,23 @@
+/*A peak element is an element that is strictly greater than its neighbors.
 
+Given an integer array nums, find a peak element, and return its index. If the array contains multiple peaks, return the index to any of the peaks.
+
+You may imagine that nums[-1] = nums[n] = -∞.
+*/
+ 
+class Solution {
+    public int findPeakElement(int[] nums) {
+        
+      int max = nums[0];
+        
+		 int mindex = 0;
+		 for(int i=0;i<nums.length;i++) {
+			 if(max<nums[i]) {
+				 max= nums[i];
+				 mindex = i;
+			 }
+		 }
+	
+         return mindex;
+    }
+}
